5c343e8351a56dd5f402d5ed2facc4f7c8809d70
Alexander Dejanovski <alex@thelastpickle.com>
2019-10-30 10:21:59 +0100

    Move integration tests to behave improve integration tests

--- cstar/jobreader.py
+++ cstar/jobreader.py
@@ -66,7 +66,7 @@ def _parse(input, file, output_directory, job, job_id, stop_after, max_days, end
     job.ssh_password = data['ssh_password']
     job.ssh_lib = data['ssh_lib']
     job.jmx_username = data['jmx_username']
-    job.mapped_variables = data['mapped_variables']
+    job.hosts_variables = data['hosts_variables']
 
     strategy = cstar.strategy.parse(state['strategy'])
     cluster_parallel = state['cluster_parallel']
